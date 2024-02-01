3a93b4366b48940e6d95630ab55f97a57d2edc32
Yakov <yakovyatchenko@yandex.ru>
2024-02-01 11:33:09 +0300

    rubocop fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'orders#index'
+
+  resources :orders, only: %i[index show]
+  post 'orders/checkout', to: 'orders#checkout'
+  get 'orders/callback', to: 'orders#callback'
 end
